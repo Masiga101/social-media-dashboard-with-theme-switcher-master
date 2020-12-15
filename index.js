6804e77e11a686bf1a79cc3f9a3516c3d771ba88
Masiga101 <60987563+Masiga101@users.noreply.github.com>
2020-12-15 23:09:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function one(){
+              document.getElementById("body").classList.toggle("body-dark");
+              for(i=1;i<=12; i++){
+                     document.getElementById("card-" + i).classList.toggle("card-dark");
+              }
+
+              for(i=1;i<=13; i++){
+                     document.getElementById("heading-" + i).classList.toggle("heading")
+              }
+              
+              document.querySelector(".instagram").attribute("id").toggle("instagram-white")
+     }
+          
+           
+           
+  
+ 
+    
+    
+
